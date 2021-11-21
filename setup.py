ede216b283c1143ba275a56fa0d545d32a9d18f0
Xander Rudelis <17013411+xrudelis@users.noreply.github.com>
2021-11-21 15:15:04 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pytrait",
-    version="0.0.1",
+    version="0.0.2",
     description="Rust-like traits for Python3",
     long_description=long_description,
     long_description_content_type="text/markdown",
